f1ee6a02ff3d4d61b849f1c994a2d0852c0a3b3b
”argnblck” <argnblck@gmail.com>
2022-04-26 14:15:12 +0300

    add some more complete lessons

---
+++ 82.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Snoop",
+    legs: 4,
+    tails: 1,
+    friends: ["Dr.Dre", "Weed"]
+  
+  
+    // Only change code above this line
+  };
