2e0f1a9f8017fa98b254aaba5c3e09857d5076ef
”argnblck” <argnblck@gmail.com>
2022-04-26 22:23:43 +0300

    add some more complete lessons

---
+++ 91.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  myMusic.push({
+    "artist": "Goldie",
+    "title": "Timeless",
+    "release_year": 1995,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  })
