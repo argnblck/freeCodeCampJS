51542f8b3a019fe1faf5b281a169a326fc1025c1
”argnblck” <argnblck@gmail.com>
2022-07-15 01:53:49 +0300

    add some complete lessons

--- 268.js
+++ 268.js
@@ -1,3 +1,6 @@
+/* Split a String into an Array Using the split Method
+Use the split method inside the splitify function to split str into an array of words. The function should return the array. Note that the words are not always separated by spaces, and the array should not contain punctuation. */
+
 const splitify = str => str.split(/\W/);
 
 splitify("Hello World,I-am code");
