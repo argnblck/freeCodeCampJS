e1e75d4ba85c2798e67b516cdd1f98edd6c68a12
”argnblck” <argnblck@gmail.com>
2022-07-21 18:37:12 +0300

    add some complete lessons

--- 211.js
+++ 211.js
@@ -1,3 +1,29 @@
 /* Return the length of the longest word in the provided sentence.
 
 Your response should be a number. */
+
+//Solution 1
+
+function findLongestWordLength1(str) {
+    let lengthLongestWord = 0;
+    str.match(/\S+/g).forEach(word => {
+      if (word.length > lengthLongestWord) {
+        lengthLongestWord = word.length;
+      }
+    })
+    return lengthLongestWord;
+  }
+  
+  console.log(findLongestWordLength1("The quick brown fox jumped over the lazy dog"));
+
+
+//Solution 2
+
+function findLongestWordLength2(str) {
+    const sortedArrOfWords = str
+    .split(" ")
+    .sort((a, b) => a.length === b.length ? 0 : a.length < b.length ? -1 : 1);
+    return sortedArrOfWords[sortedArrOfWords.length - 1].length;
+  }
+  
+  console.log(findLongestWordLength2("The quick brown fox jumped over the lazy dog"));
