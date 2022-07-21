de4af30377c89010525546556388696becb1256b
”argnblck” <argnblck@gmail.com>
2022-07-21 23:42:41 +0300

    update

--- 211.js
+++ 211.js
@@ -6,7 +6,9 @@ Your response should be a number. */
 
 function findLongestWordLength1(str) {
     let lengthLongestWord = 0;
-    str.match(/\S+/g).forEach(word => {
+    str
+    .match(/\S+/g)
+    .forEach(word => {
       if (word.length > lengthLongestWord) {
         lengthLongestWord = word.length;
       }
@@ -20,10 +22,10 @@ function findLongestWordLength1(str) {
 //Solution 2
 
 function findLongestWordLength2(str) {
-    const sortedArrOfWords = str
-    .split(" ")
-    .sort((a, b) => a.length === b.length ? 0 : a.length < b.length ? -1 : 1);
-    return sortedArrOfWords[sortedArrOfWords.length - 1].length;
+  return str
+  .split(" ")
+  .sort((a, b) => a.length === b.length ? 0 : a.length < b.length ? 1 : -1)
+  [0].length;
   }
   
   console.log(findLongestWordLength2("The quick brown fox jumped over the lazy dog"));
